a2b19ad273ced3982bd7a45985cdb163d33bcfd6
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-06 12:37:54 -0600

    Stub out goals

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/miekg/dns"
 )
 
+// Intended usage:
+//
+// dnsc -servers DNS1,DNS2,DNS3 -query server1.example.com -answer 192.168.1.250
+//
+// Should back a small table of results with a final pass/fail verdict
+
 func main() {
 	var msg dns.Msg
 	fqdn := dns.Fqdn("stacktitan.com")
