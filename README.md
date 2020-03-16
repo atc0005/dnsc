237cb21a63ac42e06fc11e4594e3ee52b5a05dbc
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-16 15:49:15 -0500

    WIP: Stubbing out next steps

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # dnsc
+
 Queries a list of DNS servers and compares responses against an expected answer
+
+## Project home
+
+See [our GitHub repo](https://github.com/atc0005/dnsc) for the latest
+code, to file an issue or submit improvements for review and potential
+inclusion into the project.
+
+## Overview
+
+Run a DNS query against all servers in a list and compare against an expected
+value. This check is most useful after moving servers between subnets when an
+IP Address change is expected.
+
+TODO:
+
+- Note available command-line flags are for options which frequently change
+- Config file for infrequently changing settings
+
+## Configuration
+
+## Examples
+
+These examples assume that the referenced `config.toml` file has the following
+contents:
+
+```toml
+
+[dns]
+
+# Multi-line array
+# https://github.com/toml-lang/toml#user-content-array
+servers = [
+    "8.8.8.8",
+    "8.8.4.4",
+]
+
+```
+
+```ShellSession
+dnsc -config-file $HOME/.config/dnsc/config.toml -q server1.example.com -e 192.168.1.250
+```
+
+```ShellSession
+dnsc -config-file $HOME/.config/dnsc/config.toml -query server1.example.com -expect 192.168.1.250
+```
+
+## Expected results
+
+- Should print small table of details with a final pass/fail verdict
+- TTL for each returned item
+- Expected answer
+- Actual answer
+
+## References
+
+- <https://github.com/toml-lang/toml#user-content-array>
+- <https://golang.org/pkg/flag/#example_>
+- <https://nostarch.com/blackhatgo>
+  - <https://github.com/blackhat-go/bhg/blob/master/ch-5/get_all_a/main.go>
--- main.go
+++ main.go
@@ -1,28 +1,79 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/dnsc
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/miekg/dns"
+	"github.com/pelletier/go-toml"
 )
 
-// Possible usage (not sure of flag names yet):
-//
-// dnsc -servers DNS1,DNS2,DNS3 -query server1.example.com -answer 192.168.1.250
-// dnsc -servers DNS1,DNS2,DNS3 -q server1.example.com -e 192.168.1.250
-// dnsc -servers DNS1,DNS2,DNS3 -ask server1.example.com -answer 192.168.1.250
-// dnsc -servers DNS1,DNS2,DNS3 -ask server1.example.com -expect 192.168.1.250
-//
-// Thus far, I like these long/short options:
-//
-// dnsc -servers DNS1,DNS2,DNS3 -q server1.example.com -e 192.168.1.250
-// dnsc -servers DNS1,DNS2,DNS3 -query server1.example.com -expect 192.168.1.250
-//
-// The "query" word is hard to avoid when it comes to DNS-related "things"
-//
-// Result: Should print small table of details with a final pass/fail verdict
+// Overridden via Makefile for release builds
+var version string = "dev build"
+
+// Primarily used with branding
+const myAppName string = "dnsc"
+const myAppURL string = "https://github.com/atc0005/dnsc"
+
+// Config is a unified set of configuration values for this application. This
+// struct is configured via command-line flags or TOML configuration file
+// provided by the user.
+type Config struct {
+	DNSServers     []string `toml:"dns_servers"`
+	Query          string   `toml:"-"`
+	ExpectedAnswer string   `toml:"-"`
+	ConfigFile     string   `toml:"-"`
+	Version        bool     `toml:"-"`
+}
 
 func main() {
+
+	const (
+		versionFlagHelp        = "Whether to display application version and then immediately exit application."
+		queryFlagHelp          = "Fully-qualified system to lookup from all provided DNS servers."
+		expectedAnswerFlagHelp = "IP Address expected as the answer from all provided DNS servers."
+	)
+
+	config := Config{}
+
+	flag.StringVar(&config.ConfigFile, "config-file", "", "Full path to optional TOML-formatted configuration file. See config.example.toml for a starter template.")
+	flag.BoolVar(&config.Version, "version", false, versionFlagHelp)
+	flag.BoolVar(&config.Version, "v", false, versionFlagHelp+" (shorthand)")
+
+	flag.StringVar(&config.Query, "query", "", queryFlagHelp)
+	flag.StringVar(&config.Query, "q", "", queryFlagHelp)
+
+	flag.StringVar(&config.ExpectedAnswer, "expect", "", expectedAnswerFlagHelp)
+	flag.StringVar(&config.ExpectedAnswer, "e", "", expectedAnswerFlagHelp)
+
+	fh, err := os.Open(config.ConfigFile)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer fh.Close()
+
+	data, err := ioutil.ReadAll(fh)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	if err := toml.Unmarshal(data, config); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
 	var msg dns.Msg
 	fqdn := dns.Fqdn("stacktitan.com")
 	msg.SetQuestion(fqdn, dns.TypeA)
