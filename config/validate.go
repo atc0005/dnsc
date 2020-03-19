4e4e592338297ed5f2eb4a6f121178d36b71e48b
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-19 14:16:29 -0500

    Split Validate() into separate file

---
+++ config/validate.go
@@ -0,0 +1,73 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/dnsc
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/apex/log"
+)
+
+// Validate verifies all struct fields have been provided acceptable values
+func (c Config) Validate() error {
+
+	// TODO: Ensure this is completely optional
+	// if c.configFile == "" {
+	// 	return fmt.Errorf("missing fully-qualified path to config file to load")
+	// }
+	// log.Debugf("c.configFile validates: %#v", c.configFile)
+
+	if c.Servers() == nil || len(c.Servers()) == 0 {
+		return fmt.Errorf("one or more DNS servers not provided")
+	}
+	log.Debugf("c.Servers() validates: (%d entries) %#v", len(c.Servers()), c.Servers())
+
+	if c.cliConfig.IgnoreDNSErrors == nil {
+		return fmt.Errorf("c.cliConfig.IgnoreDNSErrors not initialized")
+	}
+	log.Debug("c.cliConfig.IgnoreDNSErrors initialized")
+
+	if c.fileConfig.IgnoreDNSErrors == nil {
+		return fmt.Errorf("c.fileConfig.IgnoreDNSErrors not initialized")
+	}
+	log.Debug("c.fileConfig.IgnoreDNSErrors initialized")
+
+	if c.Query() == "" {
+		return fmt.Errorf("query not provided")
+	}
+	log.Debugf("c.Query() validates: %#v", c.Query())
+
+	switch c.LogLevel() {
+	case LogLevelFatal:
+	case LogLevelError:
+	case LogLevelWarn:
+	case LogLevelInfo:
+	case LogLevelDebug:
+	default:
+		return fmt.Errorf("invalid option %q provided for log level",
+			c.LogLevel())
+	}
+	log.Debugf("c.LogLevel() validates: %#v", c.LogLevel())
+
+	switch c.LogFormat() {
+	case LogFormatCLI:
+	case LogFormatJSON:
+	case LogFormatLogFmt:
+	case LogFormatText:
+	case LogFormatDiscard:
+	default:
+		return fmt.Errorf("invalid option %q provided for log format",
+			c.LogFormat())
+	}
+	log.Debugf("c.LogFormat() validates: %#v", c.LogFormat())
+
+	// Optimist
+	log.Debug("All validation checks pass")
+	return nil
+
+}
