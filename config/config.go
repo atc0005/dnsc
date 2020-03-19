5f6f8c37b91a15bf8018c84ec0ee73a9151adbbe
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-19 10:42:50 -0500

    Tweak spacing

--- config/config.go
+++ config/config.go
@@ -375,19 +375,16 @@ func (c Config) Validate() error {
 	if c.configFile == "" {
 		return fmt.Errorf("missing fully-qualified path to config file to load")
 	}
-
 	log.Debugf("c.configFile validates: %#v", c.configFile)
 
 	if c.Servers() == nil || len(c.Servers()) == 0 {
 		return fmt.Errorf("one or more DNS servers not provided")
 	}
-
 	log.Debugf("c.Servers() validates: (%d entries) %#v", len(c.Servers()), c.Servers())
 
 	if c.Query() == "" {
 		return fmt.Errorf("query not provided")
 	}
-
 	log.Debugf("c.Query() validates: %#v", c.Query())
 
 	switch c.LogLevel() {
@@ -400,7 +397,6 @@ func (c Config) Validate() error {
 		return fmt.Errorf("invalid option %q provided for log level",
 			c.LogLevel())
 	}
-
 	log.Debugf("c.LogLevel() validates: %#v", c.LogLevel())
 
 	switch c.LogFormat() {
@@ -413,7 +409,6 @@ func (c Config) Validate() error {
 		return fmt.Errorf("invalid option %q provided for log format",
 			c.LogFormat())
 	}
-
 	log.Debugf("c.LogFormat() validates: %#v", c.LogFormat())
 
 	// Optimist
